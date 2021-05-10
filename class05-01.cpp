6a6664ad69288d5ed8d7a5b780f4daca0b027400
Hugo Gallegos <hugogallegos@gmail.com>
2021-05-10 14:05:02 -0300

    class05-01 edited

--- class05-01.cpp
+++ class05-01.cpp
@@ -6,9 +6,9 @@ int main(int argc, char const *argv[])
 {
 	int A=0,B=0;
 	printf("Ingrese el valor de A: ");
-    scanf("%d",&A);
-    printf("Ingrese el valor de B: ");
-    scanf("%d",&B);
+	scanf("%d",&A);
+	printf("Ingrese el valor de B: ");
+	scanf("%d",&B);
 	if (A==B)
 	{
 		printf("A y B son iguales\n");
@@ -17,3 +17,4 @@ int main(int argc, char const *argv[])
 	
 	return 0;
 }
+
